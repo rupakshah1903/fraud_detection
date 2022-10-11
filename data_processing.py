8f8f2656f13c378368d114b59c26bacc2dd34173
rupakshah <rupakshah1992@gmail.com>
2022-10-11 21:08:48 +0530

    added a new sum

---
+++ data_processing.py
@@ -0,0 +1,14 @@
+
+print("Adding all the data processing code related to Fraud detection")
+
+
+a=5
+b=7
+
+def sum(a,b):
+    c=a+b
+    return c
+
+print("added a new function sum which is not correct")
+
+
